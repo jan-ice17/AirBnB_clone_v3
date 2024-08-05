b6b5152d32f0b106876565ac10e93b1bbcb6f490
jan-ice17 <judysjewel17@gmail.com>
2024-08-05 23:03:00 +0300

    Set up API with /status endpoint

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """This will close the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    from os import getenv
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
